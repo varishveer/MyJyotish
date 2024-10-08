57f002a07eec82f58266e723700aeff3eb787acc
varishveer <variveersingh123@gmail.com>
2024-10-08 11:09:31 +0530

    ..

---
+++ ModelAccessLayer/Models/JyotishVideosModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelAccessLayer.Models
+{
+    public class JyotishVideosModel
+    {
+
+        public int Id { get; set; }
+        public string VideoTitle { get; set; }
+        public string VideoUrl { get; set; }
+        public int JyotishId { get; set; }
+        public JyotishModel Jyotish { get; set; }
+    }
+}
