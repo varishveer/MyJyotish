71829529e8661f36ed989b64e7ad0f586ce328a2
varishveer <variveersingh123@gmail.com>
2024-10-28 19:40:00 +0530

    Payment Table Changes

---
+++ DataAccessLayer/Migrations/20241028140842_PaymentTableChanges.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentTableChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "UserPaymentRecord",
+                newName: "Message");
+
+            migrationBuilder.RenameColumn(
+                name: "Signature",
+                table: "UserPaymentRecord",
+                newName: "SignatureId");
+
+            migrationBuilder.RenameColumn(
+                name: "Signature",
+                table: "JyotishPaymentRecord",
+                newName: "SignatureId");
+
+            migrationBuilder.RenameColumn(
+                name: "JyotishName",
+                table: "JyotishPaymentRecord",
+                newName: "Message");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Method",
+                table: "UserPaymentRecord",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Method",
+                table: "JyotishPaymentRecord",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Method",
+                table: "UserPaymentRecord");
+
+            migrationBuilder.DropColumn(
+                name: "Method",
+                table: "JyotishPaymentRecord");
+
+            migrationBuilder.RenameColumn(
+                name: "SignatureId",
+                table: "UserPaymentRecord",
+                newName: "Signature");
+
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "UserPaymentRecord",
+                newName: "UserName");
+
+            migrationBuilder.RenameColumn(
+                name: "SignatureId",
+                table: "JyotishPaymentRecord",
+                newName: "Signature");
+
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "JyotishPaymentRecord",
+                newName: "JyotishName");
+        }
+    }
+}
