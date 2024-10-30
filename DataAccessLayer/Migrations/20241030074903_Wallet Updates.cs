599f76f4815d5646d59fe3abc5e39dfc624b1932
varishveer <variveersingh123@gmail.com>
2024-10-30 13:19:40 +0530

    database migration

---
+++ DataAccessLayer/Migrations/20241030074903_Wallet Updates.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class WalletUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JyotishWalletHistroy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JId = table.Column<int>(type: "int", nullable: false),
+                    amount = table.Column<long>(type: "bigint", nullable: false),
+                    PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentAction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JyotishWalletHistroy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JyotishWalletHistroy_JyotishRecords_JId",
+                        column: x => x.JId,
+                        principalTable: "JyotishRecords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserWalletHistroy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UId = table.Column<int>(type: "int", nullable: false),
+                    amount = table.Column<long>(type: "bigint", nullable: false),
+                    PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentAction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserWalletHistroy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserWalletHistroy_Users_UId",
+                        column: x => x.UId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JyotishWalletHistroy_JId",
+                table: "JyotishWalletHistroy",
+                column: "JId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserWalletHistroy_UId",
+                table: "UserWalletHistroy",
+                column: "UId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JyotishWalletHistroy");
+
+            migrationBuilder.DropTable(
+                name: "UserWalletHistroy");
+        }
+    }
+}
