885a2706bad91b77cb71028ad0311e9da7752086
varishveer <variveersingh123@gmail.com>
2024-11-07 15:24:26 +0530

    Some changes in database

---
+++ DataAccessLayer/Migrations/20241107095317_07-11-2024a.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _07112024a : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ActiveStatus",
+                table: "AppointmentSlots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActiveStatus",
+                table: "AppointmentSlots");
+        }
+    }
+}
