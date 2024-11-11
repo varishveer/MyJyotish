e932cdad0e5713de6e63c2c196356a0625b6f65f
varishveer <variveersingh123@gmail.com>
2024-11-11 10:38:28 +0530

    Some changes

---
+++ DataAccessLayer/Migrations/20241111050653_11-11-2024 some changes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _11112024somechanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentCharges",
+                table: "jyotishTempRecords",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentCharges",
+                table: "jyotishTempRecords");
+        }
+    }
+}
