3409bce57cdf3df96de73645341d4cef1cf4b8c7
varishveer <variveersingh123@gmail.com>
2024-11-18 17:03:14 +0530

    some changes

---
+++ DataAccessLayer/Migrations/20241118113207_18-11-2024.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _18112024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "JyotishUserAttachmentRecord",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "JyotishUserAttachmentRecord");
+        }
+    }
+}
