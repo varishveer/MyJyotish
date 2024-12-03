a23907bcd6e82491b5e93703172b4391121c1676
mdsahbag-dev <rudrasingh400101@gmail.com>
2024-12-03 18:49:41 +0530

    some changes

---
+++ MyJyotishJiWebDesign/Controllers/AdminEmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyJyotishGWeb.Controllers
+{
+    public class AdminEmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
