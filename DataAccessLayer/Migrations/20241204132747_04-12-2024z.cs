dca1b1f25994eaf8eae11d04f2470d5f4a85aaae
varishveer <variveersingh123@gmail.com>
2024-12-04 19:00:16 +0530

    Some changes

---
+++ DataAccessLayer/Migrations/20241204132747_04-12-2024z.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _04122024z : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InterviewMeeting",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    JyotishId = table.Column<int>(type: "int", nullable: false),
+                    ApproveStatus = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InterviewMeeting", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InterviewMeeting_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InterviewMeeting_JyotishRecords_JyotishId",
+                        column: x => x.JyotishId,
+                        principalTable: "JyotishRecords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterviewMeeting_EmployeeId",
+                table: "InterviewMeeting",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterviewMeeting_JyotishId",
+                table: "InterviewMeeting",
+                column: "JyotishId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InterviewMeeting");
+        }
+    }
+}
