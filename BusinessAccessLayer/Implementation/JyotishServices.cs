860f4cf4a6962774331f6c8023a88a5d207950bc
varishveer <variveersingh123@gmail.com>
2024-12-04 15:02:22 +0530

    Some chnages

--- BusinessAccessLayer/Implementation/JyotishServices.cs
+++ BusinessAccessLayer/Implementation/JyotishServices.cs
@@ -1821,10 +1821,17 @@ namespace BusinessAccessLayer.Implementation
        /* public JyotishDashboardDataViewModel JyotishDashboardData(int Id)
         {
             var Jyotish = _context.JyotishRecords.Where(x => x.Id == Id).FirstOrDefault();
-            if(Jyotish == null )
+            if (Jyotish == null)
             { return null; }
 
-            var Appointment = _context.AppointmentRecords.Where(x => x.JyotishId == Id).Include(x => x.UserRecord).Select(x => x.UserRecord).ToList();
+            var AppointmentClient = _context.AppointmentRecords.Where(x => x.JyotishId == Id).Include(x => x.UserRecord).Select(x => x.UserRecord.Id).Distinct().ToList();
+            var ChatClient = _context.ChatedUser.Where(x => x.JyotishId == Id).Include(x => x.User).Select(x => x.User.Id).Distinct().ToList();
+
+            var TotalClient = AppointmentClient.Union(ChatClient).ToList();
+
+            var TodayAppointment = _context.AppointmentRecords.Where(x => x.JyotishId == Id && x.Date == DateTime.Now).Count();
+            var UpcommingAppointment = _context.
+
         }*/
 
     }
