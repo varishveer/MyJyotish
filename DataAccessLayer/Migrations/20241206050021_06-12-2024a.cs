850e9b6fc2216084ffd395bca786aa0e9e581b59
varishveer <variveersingh123@gmail.com>
2024-12-06 10:32:00 +0530

    Some changes

---
+++ DataAccessLayer/Migrations/20241206050021_06-12-2024a.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _06122024a : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Incomplete",
+                table: "SlotBooking",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Incomplete",
+                table: "SlotBooking");
+        }
+    }
+}
