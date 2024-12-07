236ec556ae3ee6fe77f716add143ec40c8e1dd79
mdsahbag-dev <rudrasingh400101@gmail.com>
2024-12-07 12:37:51 +0530

    some changes

---
+++ DataAccessLayer/Migrations/20241207070648_07-12-2024y.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _07122024y : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CountryCodeId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CountryCodeId",
+                table: "Users",
+                column: "CountryCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_CountryCode_CountryCodeId",
+                table: "Users",
+                column: "CountryCodeId",
+                principalTable: "CountryCode",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_CountryCode_CountryCodeId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CountryCodeId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CountryCodeId",
+                table: "Users");
+        }
+    }
+}
