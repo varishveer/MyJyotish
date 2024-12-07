566e9d421ff65d8bdb839ec29a67bc84dcc2a557
mdsahbag-dev <rudrasingh400101@gmail.com>
2024-12-07 17:06:42 +0530

    some changes

--- MyJyotishJiWebDesign/Controllers/AdminEmployeesController.cs
+++ MyJyotishJiWebDesign/Controllers/AdminEmployeesController.cs
@@ -1,16 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyJyotishGWeb.Controllers
 {
     public class AdminEmployeesController : Controller
     {
-        
-        public IActionResult Login()
+
+    public IActionResult Login()
         {
-            return View();
-        } 
+           return View();
+        }
+
+        [HttpGet]
+        public IActionResult SetCookieAfterLogin()
+        {
+            
+               Response.Cookies.Append("role", "employee", new CookieOptions
+                {
+                    Expires = DateTimeOffset.Now.AddDays(7),
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict
+                });
+            
+            return Json(true);
+        }
+
+        [HttpGet]
+        public IActionResult logoutEmployee()
+        {
+            Response.Cookies.Delete("role");
+
+            return Json(true);
+        }
+
         public IActionResult Index()
         {
+               Response.Cookies.Append("role", "employee", new CookieOptions
+                {
+                    Expires = DateTimeOffset.Now.AddDays(7),
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict
+                });
+            
+
             return View();
         } 
        
