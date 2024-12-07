87ab55d53d6ee0ca53325feee361bbc7fec2b7f4
varishveer <variveersingh123@gmail.com>
2024-12-07 18:17:38 +0530

    Some changes

---
+++ DataAccessLayer/Migrations/20241207124644_07-12-2024e.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _07122024e : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SerialNo",
+                table: "Sliders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SerialNo",
+                table: "Sliders");
+        }
+    }
+}
