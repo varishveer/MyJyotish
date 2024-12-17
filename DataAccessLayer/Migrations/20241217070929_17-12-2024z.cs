ea833cbae74fe6e8505b2fc1c39f69c0b842d272
mdsahbag-dev <rudrasingh400101@gmail.com>
2024-12-17 12:40:53 +0530

    some changes

---
+++ DataAccessLayer/Migrations/20241217070929_17-12-2024z.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _17122024z : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JId",
+                table: "ClientMembers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientMembers_JId",
+                table: "ClientMembers",
+                column: "JId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ClientMembers_JyotishRecords_JId",
+                table: "ClientMembers",
+                column: "JId",
+                principalTable: "JyotishRecords",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ClientMembers_JyotishRecords_JId",
+                table: "ClientMembers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClientMembers_JId",
+                table: "ClientMembers");
+
+            migrationBuilder.DropColumn(
+                name: "JId",
+                table: "ClientMembers");
+        }
+    }
+}
