1f75f20ae84576629c082d417c618204645be6c8
mdsahbag-dev <rudrasingh400101@gmail.com>
2024-12-31 18:08:59 +0530

    some changes

--- MyJyotishJi/Controllers/CallController.cs
+++ MyJyotishJi/Controllers/CallController.cs
@@ -1,7 +1,12 @@
-﻿using BusinessAccessLayer.Abstraction;
+﻿using Azure.Core;
+using BusinessAccessLayer.Abstraction;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ModelAccessLayer.ViewModels;
+using Twilio;
+using Twilio.Jwt.AccessToken;
+using Twilio.TwiML;
+using Twilio.Types;
 
 namespace MyJyotishGApi.Controllers
 {
@@ -9,25 +14,34 @@ namespace MyJyotishGApi.Controllers
     [ApiController]
     public class CallController : ControllerBase
     {
-        /*private readonly ICallServices _callService;
+        [HttpGet("accessToken")]
 
-        public CallController(ICallServices callService)
+        public IActionResult GenerateToken(string identity)
         {
-            _callService = callService;
+            const string twilioAccountSid = "AC69120ba0ee6d3c27dc72e8d3385c798e";
+            const string twilioApiKey = "SK9d52035ce4a96de77326d79095741a4d";
+            const string twilioApiSecret = "nUZk65XltHpTQxSdqwhBEpkbwgZ6w4y7";
+            TwilioClient.Init(twilioApiKey, twilioApiSecret);
+            var grants = new HashSet<IGrant>
+        {
+            new VoiceGrant
+            {
+                IncomingAllow = true,
+                OutgoingApplicationSid = "AP8e437be7b812411488baf1ff2335baa9"
+            }
+        };
+
+            var token = new Token(
+                twilioAccountSid,
+                twilioApiKey,
+                twilioApiSecret,
+                identity,
+                DateTime.UtcNow.AddMinutes(30),
+                grants: grants
+            );
+            return Ok(new { status=200,message="Token generated successfully",token=token.ToJwt()});
         }
 
-        [HttpPost("join")]
-        public async Task<IActionResult> JoinCall([FromBody] CallViewModel model)
-        {
-            await _callService.JoinCallAsync(model.RoomName, model.UserId);
-            return Ok(new { message = "Joined successfully" });
-        }
-
-        [HttpPost("leave")]
-        public async Task<IActionResult> LeaveCall([FromBody] CallViewModel model)
-        {
-            await _callService.LeaveCallAsync(model.RoomName, model.UserId);
-            return Ok(new { message = "Left successfully" });
-        }*/
     }
+
 }
