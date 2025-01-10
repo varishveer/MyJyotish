fc6a10adf27788a2e88f17ed422ee17f7687d78a
varishveer <variveersingh123@gmail.com>
2025-01-10 10:28:27 +0530

    some changes

--- MyJyotishJi/Controllers/ChatController.cs
+++ MyJyotishJi/Controllers/ChatController.cs
@@ -329,6 +329,7 @@ namespace MyJyotishGApi.Controllers
 
 
 
+                    
                     if (result.CloseStatus.HasValue)
                     {
                         if (sendBy == "client")
